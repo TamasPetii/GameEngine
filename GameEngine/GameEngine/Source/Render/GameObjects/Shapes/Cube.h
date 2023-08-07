e35e40a8c23139fba6a90b78a3039a56d0d2180d
TamasPeti <gr1zh1@inf.elte.hu>
2023-08-07 23:28:18 +0200

    Impelemted a Cube mesh

---
+++ GameEngine/GameEngine/Source/Render/GameObjects/Shapes/Cube.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "../GameObject.h"
+
+class Cube : public GameObject
+{
+public:
+	Cube();
+	~Cube();
+	void Render() override;
+
+	inline VertexArrayObject* GetVao() const { return mVao; }
+	inline VertexBufferObject* GetVbo() const { return mVbo; }
+	inline IndexBufferObject* GetIbo() const { return mIbo; }
+private:
+	void CreateBuffers();
+	void UploadBuffers();
+	VertexArrayObject* mVao;
+	VertexBufferObject* mVbo;
+	IndexBufferObject* mIbo;
+	unsigned int mIndexCount;
+	unsigned int mVertexCount;
+};
+
