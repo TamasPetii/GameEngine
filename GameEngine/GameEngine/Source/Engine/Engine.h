14bdb63683a70d1247366f4c4465d3174cbede0b
TamasPeti <gr1zh1@inf.elte.hu>
2023-08-07 23:26:38 +0200

    Created an Engine header containing all the abstractions

---
+++ GameEngine/GameEngine/Source/Engine/Engine.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Abstraction/Buffer.h"
+#include "Abstraction/Camera.h"
+#include "Abstraction/FrameBuffer.h"
+#include "Abstraction/Program.h"
+#include "Abstraction/Shader.h"
+#include "Abstraction/Texture.h"
+#include "Abstraction/Vertex.h"
+#include "Abstraction/VertexArrayObject.h"
