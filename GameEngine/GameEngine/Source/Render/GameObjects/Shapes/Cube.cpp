249047a0b71f2f32fcdc5f1514f7e70b088a1fb4
TamasPeti <gr1zh1@inf.elte.hu>
2023-08-09 10:23:22 +0200

    Implemented a cube mesh

--- GameEngine/GameEngine/Source/Render/GameObjects/Shapes/Cube.cpp
+++ GameEngine/GameEngine/Source/Render/GameObjects/Shapes/Cube.cpp
@@ -1,99 +1,62 @@
 #include "Cube.h"
 
-Cube::Cube() : GameObject()
+Cube::Cube() : Shape()
 {
 	mName = "Cube";
-	CreateBuffers();
+	CreateVertices();
+	CreateIndices();
 	UploadBuffers();
 }
 
-Cube::~Cube()
+void Cube::CreateVertices()
 {
-	delete mVao;
-	delete mIbo;
-	delete mVbo;
-}
-
-void Cube::CreateBuffers()
-{
-	mVao = new VertexArrayObject();
-	mIbo = new IndexBufferObject();
-	mVbo = new VertexBufferObject();
-}
-
-void Cube::UploadBuffers()
-{
-	std::vector<Vertex> vertices;
-
+	mVertices.clear();
 	//Front
-	vertices.push_back({ glm::vec3(-1, 1, -1), glm::vec3(0, 0, -1), glm::vec2(0, 1) });
-	vertices.push_back({ glm::vec3(-1, -1, -1), glm::vec3(0, 0, -1), glm::vec2(0, 0) });
-	vertices.push_back({ glm::vec3(1, -1, -1), glm::vec3(0, 0, -1), glm::vec2(1, 0) });
-	vertices.push_back({ glm::vec3(1, 1, -1), glm::vec3(0, 0, -1), glm::vec2(1, 1) });
+	mVertices = std::vector<Vertex>
+	{
 
-	//Back
-	vertices.push_back({ glm::vec3(1, 1, 1), glm::vec3(0, 0, 1), glm::vec2(0, 1) });
-	vertices.push_back({ glm::vec3(1, -1, 1), glm::vec3(0, 0, 1), glm::vec2(0, 0) });
-	vertices.push_back({ glm::vec3(-1, -1, 1), glm::vec3(0, 0, 1), glm::vec2(1, 0) });
-	vertices.push_back({ glm::vec3(-1, 1, 1), glm::vec3(0, 0, 1), glm::vec2(1, 1) });
+		{ glm::vec3(-1, 1, 1), glm::vec3(0, 0, -1), glm::vec2(0, 1) },
+		{ glm::vec3(-1, -1, 1), glm::vec3(0, 0, -1), glm::vec2(0, 0) },
+		{ glm::vec3(1, -1, 1), glm::vec3(0, 0, -1), glm::vec2(1, 0) },
+		{ glm::vec3(1, 1, 1), glm::vec3(0, 0, -1), glm::vec2(1, 1) },
 
-	//Left
-	vertices.push_back({ glm::vec3(-1, 1, 1), glm::vec3(-1, 0, 0), glm::vec2(0, 1) });
-	vertices.push_back({ glm::vec3(-1, -1, 1), glm::vec3(-1, 0, 0), glm::vec2(0, 0) });
-	vertices.push_back({ glm::vec3(-1, -1, -1), glm::vec3(-1, 0, 0), glm::vec2(1, 0) });
-	vertices.push_back({ glm::vec3(-1, 1, -1), glm::vec3(-1, 0, 0), glm::vec2(1, 1) });
+		{ glm::vec3(1, 1, -1), glm::vec3(0, 0, 1), glm::vec2(0, 1) },
+		{ glm::vec3(1, -1, -1), glm::vec3(0, 0, 1), glm::vec2(0, 0) },
+		{ glm::vec3(-1, -1, -1), glm::vec3(0, 0, 1), glm::vec2(1, 0) },
+		{ glm::vec3(-1, 1, -1), glm::vec3(0, 0, 1), glm::vec2(1, 1) },
 
-	//Right
-	vertices.push_back({ glm::vec3(1, 1, -1), glm::vec3(1, 0, 0), glm::vec2(0, 1) });
-	vertices.push_back({ glm::vec3(1, -1, -1), glm::vec3(1, 0, 0), glm::vec2(0, 0) });
-	vertices.push_back({ glm::vec3(1, -1, 1), glm::vec3(1, 0, 0), glm::vec2(1, 0) });
-	vertices.push_back({ glm::vec3(1, 1, 1), glm::vec3(1, 0, 0), glm::vec2(1, 1) });
+		{ glm::vec3(-1, 1, -1), glm::vec3(-1, 0, 0), glm::vec2(0, 1) },
+		{ glm::vec3(-1, -1, -1), glm::vec3(-1, 0, 0), glm::vec2(0, 0) },
+		{ glm::vec3(-1, -1, 1), glm::vec3(-1, 0, 0), glm::vec2(1, 0) },
+		{ glm::vec3(-1, 1, 1), glm::vec3(-1, 0, 0), glm::vec2(1, 1) },
 
-	//Top
-	vertices.push_back({ glm::vec3(-1, 1, 1), glm::vec3(0, 1, 0), glm::vec2(0, 1) });
-	vertices.push_back({ glm::vec3(-1, 1, -1), glm::vec3(0, 1, 0), glm::vec2(0, 0) });
-	vertices.push_back({ glm::vec3(1, 1, -1), glm::vec3(0, 1, 0), glm::vec2(1, 0) });
-	vertices.push_back({ glm::vec3(1, 1, 1), glm::vec3(0, 1, 0), glm::vec2(1, 1) });
+		{ glm::vec3(1, 1, 1), glm::vec3(1, 0, 0), glm::vec2(0, 1) },
+		{ glm::vec3(1, -1, 1), glm::vec3(1, 0, 0), glm::vec2(0, 0) },
+		{ glm::vec3(1, -1, -1), glm::vec3(1, 0, 0), glm::vec2(1, 0) },
+		{ glm::vec3(1, 1, -1), glm::vec3(1, 0, 0), glm::vec2(1, 1) },
 
-	//Bottom
-	vertices.push_back({ glm::vec3(-1, -1, -1), glm::vec3(0, -1, 0), glm::vec2(0, 1) });
-	vertices.push_back({ glm::vec3(-1, -1, 1), glm::vec3(0, -1, 0), glm::vec2(0, 0) });
-	vertices.push_back({ glm::vec3(1, -1, 1), glm::vec3(0, -1, 0), glm::vec2(1, 0) });
-	vertices.push_back({ glm::vec3(1, -1, -1), glm::vec3(0, -1, 0), glm::vec2(1, 1) });
+		{ glm::vec3(-1, 1, -1), glm::vec3(0, 1, 0), glm::vec2(0, 1) },
+		{ glm::vec3(-1, 1, 1), glm::vec3(0, 1, 0), glm::vec2(0, 0) },
+		{ glm::vec3(1, 1, 1), glm::vec3(0, 1, 0), glm::vec2(1, 0) },
+		{ glm::vec3(1, 1, -1), glm::vec3(0, 1, 0), glm::vec2(1, 1) },
 
-	std::vector<unsigned int> indices;
+		{ glm::vec3(-1, -1, 1), glm::vec3(0, -1, 0), glm::vec2(0, 1) },
+		{ glm::vec3(-1, -1, -1), glm::vec3(0, -1, 0), glm::vec2(0, 0) },
+		{ glm::vec3(1, -1, -1), glm::vec3(0, -1, 0), glm::vec2(1, 0) },
+		{ glm::vec3(1, -1, 1), glm::vec3(0, -1, 0), glm::vec2(1, 1) }
+	};
+}
 
+void Cube::CreateIndices()
+{
+	mIndices.clear();
 	for (unsigned int i = 0; i < 24; i += 4)
 	{
-		indices.push_back(0 + i);
-		indices.push_back(2 + i);
-		indices.push_back(1 + i);
-		indices.push_back(2 + i);
-		indices.push_back(0 + i);
-		indices.push_back(3 + i);
+		mIndices.push_back(0 + i);
+		mIndices.push_back(1 + i);
+		mIndices.push_back(2 + i);
+		mIndices.push_back(0 + i);
+		mIndices.push_back(2 + i);
+		mIndices.push_back(3 + i);
 	}
-
-	mIndexCount = indices.size();
-	mVertexCount = vertices.size();
-
-	std::cout << mIndexCount << " " << mVertexCount << std::endl;
-
-	mVao->Bind();
-	mVbo->AttachData(vertices, GL_STATIC_DRAW);
-	mIbo->AttachData(indices, GL_STATIC_DRAW);
-
-	mVao->LinkAttribute(*mVbo, 0, 3, GL_FLOAT, sizeof(Vertex), nullptr);
-	mVao->LinkAttribute(*mVbo, 1, 3, GL_FLOAT, sizeof(Vertex), (const void*)(1 * sizeof(glm::vec3)));
-	mVao->LinkAttribute(*mVbo, 2, 2, GL_FLOAT, sizeof(Vertex), (const void*)(2 * sizeof(glm::vec3)));
-
-	mVao->UnBind();
-	mVbo->UnBind();
-	mIbo->UnBind();
-}
-
-void Cube::Render()
-{
-	mVao->Bind();
-	glDrawElements(GL_TRIANGLES, mIndexCount, GL_UNSIGNED_INT, nullptr);
-	mVao->UnBind();
 }
--- GameEngine/GameEngine/Source/Render/GameObjects/Shapes/Cube.h
+++ GameEngine/GameEngine/Source/Render/GameObjects/Shapes/Cube.h
@@ -1,22 +1,11 @@
 #pragma once
-#include "../GameObject.h"
+#include "Shape.h"
 
-class Cube : public GameObject
+class Cube : public Shape
 {
 public:
 	Cube();
-	~Cube();
-	void Render() override;
-	inline VertexArrayObject* GetVao() const { return mVao; }
-	inline VertexBufferObject* GetVbo() const { return mVbo; }
-	inline IndexBufferObject* GetIbo() const { return mIbo; }
-private:
-	void CreateBuffers();
-	void UploadBuffers();
-	VertexArrayObject* mVao;
-	VertexBufferObject* mVbo;
-	IndexBufferObject* mIbo;
-	unsigned int mIndexCount;
-	unsigned int mVertexCount;
+	void CreateVertices() override;
+	void CreateIndices() override;
 };
 
