ec1988f33e51e27f76e28bca1460b50094884d91
TamasPeti <gr1zh1@inf.elte.hu>
2023-08-10 23:26:15 +0200

    Directional light class handles ortographics proejction for shadow mapping

--- GameEngine/GameEngine/Source/Render/GameObjects/Lights/DirectionLight.h
+++ GameEngine/GameEngine/Source/Render/GameObjects/Lights/DirectionLight.h
@@ -9,8 +9,15 @@ public:
 	void Render() override {}
 
 	//Getter
+	glm::mat4 GetViewProjMatrix();
 	inline glm::vec3& GetDirectionRef() { return mDirection; }
-private:
+	glm::mat4 mProjMatrix;
+	glm::mat4 mViewMatrix;
+	glm::vec3 mPosition;
 	glm::vec3 mDirection;
+	glm::vec2 mProjX;
+	glm::vec2 mProjY;
+	glm::vec2 mProjZ;
+private:
 };
 
