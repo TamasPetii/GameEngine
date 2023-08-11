60534756aa1b93c0ca8f68c9f2edb5bfed9dcde0
TamasPeti <gr1zh1@inf.elte.hu>
2023-08-12 00:38:34 +0200

    MeshComponent contains color and texture data

--- GameEngine/GameEngine/Source/Engine/Components/Mesh/MeshComponent.cpp
+++ GameEngine/GameEngine/Source/Engine/Components/Mesh/MeshComponent.cpp
@@ -1,5 +1,16 @@
 #include "MeshComponent.h"
 
+MeshComponent::MeshComponent()
+{
+	mColor = glm::vec3(1);
+	mTexture = nullptr;
+}
+
+MeshComponent::~MeshComponent()
+{
+	delete mMesh;
+}
+
 void MeshComponent::Render()
 {
 	mMesh->Render();
--- GameEngine/GameEngine/Source/Engine/Components/Mesh/MeshComponent.h
+++ GameEngine/GameEngine/Source/Engine/Components/Mesh/MeshComponent.h
@@ -5,9 +5,16 @@
 class MeshComponent : public Component
 {
 public:
+	MeshComponent();
+	~MeshComponent(); //TODO: Delete mesh
 	void Render();
 	void AttachMesh(IMesh* mesh) { this->mMesh = mesh; }
+	inline glm::vec3& GetColor() { return mColor; }
+	inline Texture2D* GetTexture() { return mTexture; }
+	inline void SetTexture(Texture2D* texture) { mTexture = texture; }
 private:
 	IMesh* mMesh;
+	glm::vec3 mColor;
+	Texture2D* mTexture;
 };
 
