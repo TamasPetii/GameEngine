24621f07cf39fcca9a4d7baf4549ffe059bf54ef
TamasPeti <gr1zh1@inf.elte.hu>
2023-08-11 21:52:43 +0200

    Light template structure system implemented

---
+++ GameEngine/GameEngine/Source/Engine/Components/Light/LightSource/Light.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <GLM/glm.hpp>
+
+class ILight
+{
+public:
+	virtual glm::vec3& GetColor() = 0;
+	virtual float& GetDiffuseIntensity() = 0;
+	virtual float& GetSpecularIntensity() = 0;
+};
+
+class LightBase : ILight
+{
+public:
+	glm::vec3& GetColor() override { return mColor; }
+	float& GetDiffuseIntensity() override { return mDiffuseIntensity; }
+	float& GetSpecularIntensity() override { return mSpecularIntensity; }
+protected:
+	glm::vec3 mColor;
+	float mDiffuseIntensity;
+	float mSpecularIntensity;
+};
+
+template<typename T>
+class Light {};
