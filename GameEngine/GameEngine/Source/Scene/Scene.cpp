094a3fd0f4171a338a0120b807386edc5d9806c2
TamasPeti <gr1zh1@inf.elte.hu>
2023-08-31 19:20:34 +0200

    Scene class implemented
    - Scene object holds now the entities.

--- GameEngine/GameEngine/Source/Scene/Scene.cpp
+++ GameEngine/GameEngine/Source/Scene/Scene.cpp
@@ -1,6 +1,20 @@
 #include "Scene.h"
 
-/*
+Scene::Scene()
+{
+	m_Name = "MainScene";
+}
+
+Scene::Scene(const json& object)
+{
+	LoadFromJson(object);
+}
+
+Scene::~Scene()
+{
+
+}
+
 void Scene::AttachEntity(Entity* entity)
 {
 	m_EntityList.push_back(entity);
@@ -13,4 +27,48 @@ void Scene::DetachEntity(Entity* entity)
 	if (it != m_EntityList.end())
 		m_EntityList.erase(it);
 }
-*/
+
+void Scene::DeleteEntity(Entity* entity)
+{
+	m_ToDelete.push_back(entity);
+}
+
+void Scene::OnStart()
+{
+
+}
+
+void Scene::OnUpdate()
+{
+	for (auto entity : m_ToDelete)
+	{
+		DetachEntity(entity);
+		delete entity;
+	}
+	m_ToDelete.clear();
+}
+
+
+json Scene::SaveToJson() const
+{
+	json data;
+	data["Scene"]["Name"] = m_Name;
+	data["Scene"]["Entities"] = json::array();
+
+	for (auto entity : m_EntityList)
+	{
+		data["Scene"]["Entities"].push_back(entity->SaveToJson());
+	}
+	
+	return data;
+}
+
+void Scene::LoadFromJson(const json& object)
+{
+	m_Name = object["Name"];
+	
+	for (const auto& entity : object["Entities"])
+	{
+		AttachEntity(new Entity(entity["Entity"]));
+	}
+}
--- GameEngine/GameEngine/Source/Scene/Scene.h
+++ GameEngine/GameEngine/Source/Scene/Scene.h
@@ -1,16 +1,37 @@
 #pragma once
 #include "../JsonObject.h"
 #include "../Engine/Engine.h"
-/*
 #include "Entity/Entity.h"
 
-class Scene
+class Scene : public JsonObject
 {
 public:
+	json SaveToJson() const override;
+	void LoadFromJson(const json& object) override;
+
+	Scene();
+	Scene(const json& object);
+	~Scene();
+
+	void OnStart();
+	void OnUpdate();
+
 	void AttachEntity(Entity* entity);
 	void DetachEntity(Entity* entity);
+	void DeleteEntity(Entity* entity);
+
+	/*Getter*/
+	inline const auto& Get_Name() const { return m_Name; }
+	inline const auto& Get_EntityList() const { return m_EntityList; }
+	inline const auto& Get_ActiveEntity() const { return m_ActiveEntity; }
+	/*Reference*/
+	inline auto& Ref_Name() { return m_Name; }
+	inline auto& Ref_EntityList() { return m_EntityList; }
+	inline auto& Ref_ActiveEntity() { return m_ActiveEntity; }
 private:
+	std::string m_Name;
+	Entity* m_ActiveEntity;
 	std::vector<Entity*> m_EntityList;
+	std::vector<Entity*> m_ToDelete;
 };
 
-*/
