fa66c99b8a1d8e96036607a967201fba27a92fb5
TamasPeti <gr1zh1@inf.elte.hu>
2023-09-08 01:02:53 +0200

    [GE-8] : Refactored ProgramObject

---
+++ GameEngine/GameEngine/Source/Engine/Abstraction/ProgramObject.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "Bindable.h"
+#include "ShaderObject.h"
+#include <GLM/glm.hpp>
+#include <iostream>
+#include <unordered_map>
+#include "../Utility/ImageTexture.h"
+
+namespace OpenGL::Classic
+{
+	class ProgramObject : public Bindable
+	{
+	public:
+		ProgramObject();
+		ProgramObject(const std::vector<ShaderObject>& shaders, const std::vector<ShaderObjectInfo>& infos);
+		~ProgramObject();
+		void GenerateProgram();
+		void DeleteProgram();
+		void Bind() const override;
+		void UnBind() const override;
+		void AttachShader(const ShaderObject& shader);
+		void AttachShaderInfo(const ShaderObjectInfo& info);
+
+		template<typename T>
+		inline void SetUniform(const std::string& name, const T& value);
+		void SetUniformTexture(const std::string& name, const int sampler, GLuint id, GLuint type);
+		void SetUniformTexture(const std::string& name, const int sampler, const ImageTexture* texture);
+	protected:
+		GLint GetUniformLocation(const std::string& name);
+	private:
+		GLuint m_ProgramId;
+		std::vector<ShaderObject> m_Shaders;
+		std::vector<ShaderObjectInfo> m_ShaderInfos;
+		std::unordered_map<std::string, GLint> m_UniformLocations;
+	};
+}
+
+#include "ProgramObject.inl"
